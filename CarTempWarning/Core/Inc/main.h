4f2825bf8292edc2ca0c2df8c3849ab95539a271
tubohu <dengyangfan1203@outlook.com>
2024-09-12 22:47:49 +0800

    init IO

---
+++ CarTempWarning/Core/Inc/main.h
@@ -0,0 +1,91 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2024 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f1xx_hal.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define KeyUSER_Pin GPIO_PIN_0
+#define KeyUSER_GPIO_Port GPIOA
+#define Key1_Pin GPIO_PIN_1
+#define Key1_GPIO_Port GPIOA
+#define Key2_Pin GPIO_PIN_2
+#define Key2_GPIO_Port GPIOA
+#define Key3_Pin GPIO_PIN_3
+#define Key3_GPIO_Port GPIOA
+#define Key4_Pin GPIO_PIN_4
+#define Key4_GPIO_Port GPIOA
+#define OLEDRES_Pin GPIO_PIN_6
+#define OLEDRES_GPIO_Port GPIOA
+#define OLEDDC_Pin GPIO_PIN_0
+#define OLEDDC_GPIO_Port GPIOB
+#define PeopleDetect_Pin GPIO_PIN_1
+#define PeopleDetect_GPIO_Port GPIOB
+#define LEDB_Pin GPIO_PIN_2
+#define LEDB_GPIO_Port GPIOB
+#define Buzzer_Pin GPIO_PIN_10
+#define Buzzer_GPIO_Port GPIOB
+#define DS18B20_Pin GPIO_PIN_11
+#define DS18B20_GPIO_Port GPIOB
+
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
